f7e87715b70f97b7a684a5c9a3d7a2a2db6b3a22
Nikhil Kumar <nikhilkumar@expediagroup.com>
2022-05-16 18:19:20 +0530

    Initializing Project
    Signed-off-by: Nikhil Kumar <nikhilkumar@expediagroup.com>

---
+++ Tetris/Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by Nikhil Kumar on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
