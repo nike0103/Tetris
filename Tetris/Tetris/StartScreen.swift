26a722c337bc50c24d2ac5e699545b137eb057c3
Megha Sharma <meghsharma@expediagroup.com>
2022-06-09 10:52:18 +0530

    clean code

--- Tetris/Tetris/StartScreen.swift
+++ Tetris/Tetris/StartScreen.swift
@@ -7,25 +7,14 @@
 
 import SwiftUI
 
-struct GrowingButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color(red: 0, green: 0, blue: 0.5))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-            .scaleEffect(configuration.isPressed ? 1.2 : 1)
-            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
-    }
-}
-
 struct StartScreen: View {
     @State var userName = "USER1234"
     @State private var animateGradient = true
+    var highScore = 1080
     
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.purple, .yellow], startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(colors: [ColourTheme.primaryColor, ColourTheme.secondaryColor], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
                 .onAppear {
                         withAnimation(.linear(duration: 2.0).repeatForever(autoreverses: true)) {
@@ -34,14 +23,11 @@ struct StartScreen: View {
                 }
             VStack{
                 Text("High Score").font(.headline).padding()
-                Text("1080").font(.largeTitle).padding()
+                Text("\(highScore)").font(.largeTitle).padding()
 
                 Text("Welcome \(userName)").font(.largeTitle).padding()
                 TextField("Placeholder", text: $userName).multilineTextAlignment(.center).padding()
-                Button("Start Game") {
-                    print("Start Game")
-                }
-                .buttonStyle(GrowingButton())
+                TetrisButtonView(buttonTitle: "Start Game")
             }
         }
     }
--- Tetris/Tetris/TetrisButtonView.swift
+++ Tetris/Tetris/TetrisButtonView.swift
@@ -1,11 +1,11 @@
 //
-//  TetrisButton.swift
+//  TetrisButtonView.swift
 //  Tetris
 //
 //  Created by Megha Sharma on 09/06/22.
 //
 
-import Foundation
+import SwiftUI
 
 struct GrowingButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
@@ -16,5 +16,23 @@ struct GrowingButton: ButtonStyle {
             .clipShape(Capsule())
             .scaleEffect(configuration.isPressed ? 1.2 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct TetrisButtonView: View {
+    let buttonTitle: String
+    var body: some View {
+        Button {
+            print(buttonTitle)
+        } label: {
+            Text(buttonTitle)
+        }
+        .buttonStyle(GrowingButton())
+    }
+}
+
+struct TetrisButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        TetrisButtonView(buttonTitle: "Hello World")
     }
 }
