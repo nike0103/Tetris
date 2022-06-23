4e8a9c0cd72a54cd516eab049252bd6ee1b3d039
Nidhi <nidhjain@expediagroup.com>
2022-06-23 12:23:52 +0530

    Added BlocksManager

--- Tetris/Tetris/Block.swift
+++ Tetris/Tetris/Block.swift
@@ -1,66 +1,17 @@
 import Foundation
 
-enum DirectionEnum {
-    case up, down, left, right
-}
-
-enum BlockShape {
-    case L, Z, Box, I, T
-}
-
 protocol BlockProtocol {
     var matrix: [[Int]] { get }
-    func rotateMatrix(direction: DirectionEnum)
+    func rotateMatrix()
 }
 
-class LBlock: BlockProtocol{
-    var matrix: [[Int]] = [ [1, 0],
-                            [1, 0],
-                            [1, 1]]
+enum BlockShape: CaseIterable  {
+    case L, Z, Box, I, T
     
-    func rotateMatrix(direction: DirectionEnum) {}
-}
-
-class ZBlock: BlockProtocol{
-    var matrix: [[Int]] = [[1,1,0],
-                           [0,1,0],
-                           [0,1,1]]
-    
-    func rotateMatrix(direction: DirectionEnum) {}
-}
-
-class BoxBlock: BlockProtocol{
-    var matrix: [[Int]] = [[1, 1],
-                           [1, 1]]
-    
-    func rotateMatrix(direction: DirectionEnum) {}
-}
-
-class IBlock: BlockProtocol{
-    var matrix: [[Int]] = [[0, 1],
-                           [0, 1],
-                           [0, 1],
-                           [0, 1]]
-    
-    func rotateMatrix(direction: DirectionEnum) {}
-}
-
-class TBlock: BlockProtocol{
-    var matrix: [[Int]] = [[1,1,1],
-                           [0,1,0],
-                           [0,1,0]]
-    
-    func rotateMatrix(direction: DirectionEnum) {
-        
-    }
-}
-
-class BlocksGenerator {
-    
-    func getBlocks(type: BlockShape)-> BlockProtocol {
-        switch type {
+    var block: BlockProtocol {
+        switch self {
         case .L:
-            return LBlock()
+                return LBlock()
         case .Z:
             return ZBlock()
         case .Box:
@@ -71,8 +22,45 @@ class BlocksGenerator {
             return TBlock()
         }
     }
-    
-    func getRandomBlocks(shape: BlockShape) {
-        //Will return block of random shape like L, Z, Box, I, T
-    }
 }
+
+class LBlock: BlockProtocol{
+    var matrix: [[Int]] = [ [1, 0],
+                            [1, 0],
+                            [1, 1]]
+    
+    func rotateMatrix() {}
+}
+
+class ZBlock: BlockProtocol{
+    var matrix: [[Int]] = [[1,1,0],
+                           [0,1,0],
+                           [0,1,1]]
+    
+    func rotateMatrix() {}
+}
+
+class BoxBlock: BlockProtocol{
+    var matrix: [[Int]] = [[1, 1],
+                           [1, 1]]
+    
+    func rotateMatrix() {}
+}
+
+class IBlock: BlockProtocol{
+    var matrix: [[Int]] = [[0, 1],
+                           [0, 1],
+                           [0, 1],
+                           [0, 1]]
+    
+    func rotateMatrix() {}
+}
+
+class TBlock: BlockProtocol{
+    var matrix: [[Int]] = [[1,1,1],
+                           [0,1,0],
+                           [0,1,0]]
+    
+    func rotateMatrix() {}
+}
+
